76cbcaa6134e21abe4a1c3f66d55ccf10cbc39e3
budilmk <budilmk@gmail.com>
2023-07-23 18:33:53 +0800

    Serilog and HttpLogging

--- ClinicApp.Bootstrapper/Program.cs
+++ ClinicApp.Bootstrapper/Program.cs
@@ -1,8 +1,22 @@
 using Authentication.API;
 using Booking.API;
 using Booking.Infrastructure.Database;
+using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, configuration) =>
+{
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services);
+});
+
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+});
 
 builder.Services
     .AddBookingModule()
